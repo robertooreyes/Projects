5961841b7303b8bb9f4007a7b2c9c6a3444907ea
Roberto Reyes Noguera <55290565+robertooreyes@users.noreply.github.com>
2021-11-10 14:03:55 -0500

    Add files via upload

---
+++ Web Design/Peninsula Cafe/script.js
@@ -0,0 +1,14 @@
+// Generate style for navigation bar
+
+var items = document.getElementsByClassName('baritem');
+var i;
+
+for (i = 0; i < items.length; i++) {
+	items[i].className += ' w3-col s3 w3-button w3-round w3-block w3-sand w3-hover-blue-grey';
+}
+
+function SendQuote() {
+	var name, phone, email, partysize, date;
+
+	name = document.getElementsById("");
+}
